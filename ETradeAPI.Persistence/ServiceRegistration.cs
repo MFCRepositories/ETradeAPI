4eebb278a15dbbade3845871c87ae01b7850a82c
MFCRepositories <MFCRepositories@users.noreply.github.com>
2022-04-23 16:38:04 +0300

    Add project files.

---
+++ ETradeAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETradeAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETradeAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this  IServiceCollection services)
+        {
+
+            services.AddDbContext<ETradeAPIDbContext>(
+
+                options => options.UseNpgsql(Configuration.ConnectionString)
+
+            );
+        }
+    }
+}
